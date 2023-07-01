91c7fb7cf2295202cc1dd9388994213383e80b5d
Greg T. Wallace <greg@gregtwallace.com>
2023-07-01 13:16:34 -0400

    windows: rework logic to prevent expiring

--- lego-windows-plain.ps1
+++ lego-windows-plain.ps1
@@ -1,7 +1,7 @@
 # Created by Kody Salak (@KodySalak)
 # Certhub Certificate Fetcher/Importer that is somewhat automated
 # Grabs certificate and key from CertHub, converts it to a PKCS12, and imports to the System Personal Certificate Store in Windows.
-# Version 1.1
+# Version 2.0
 
 # This script assumes ZERO liability for services that it may affect.
 # Please add more error handling if you're using in production.
@@ -26,39 +26,61 @@ $PKCS12Password = "Password"
 $EncryptedPassword = ConvertTo-SecureString -String $PKCS12Password -Force -AsPlainText
 $CertificateAPIURL = "legocerthub/api/v1/download/certificates/$CertHubCertName"
 $KeyAPIURL = "legocerthub/api/v1/download/privatekeys/$KeyName"
-$CurrentCertExpireTime = (Get-ChildItem -Path "Cert:\LocalMachine\My" | Where-Object {$_.Subject -Like "*$CertSubject"}).NotAfter
-$CurrentTime = Get-Date
+$CurrentCertExpireTime = (Get-ChildItem -Path "Cert:\LocalMachine\My" | Where-Object { $_.Subject -Like "*$CertSubject" }).NotAfter
 $CertPath = "$TempCerts\certchain.crt"
 $KeyPath = "$TempCerts\key.key"
 $PKCS12Path = "$TempCerts\output.pfx"
 
+###
+# Exit Func for errors (also removes temp folder)
+function Exit-Failed {
+    Remove-Item -Recurse -Force $TempCerts
+    Exit 1
+}
+
 # Main
-If ($CurrentCertExpireTime -lt $CurrentTime -Or [string]::IsNullOrWhiteSpace($CurrentCertExpireTime)) {
-    Write-Host "Certificate Expired, re-registering"
-    # Make folder if it doesn't exist
-    If (!(Get-ChildItem $TempCerts -ErrorAction SilentlyContinue)) {
+# Make temp folder, if it doesn't exist
+If (!(Get-ChildItem $TempCerts -ErrorAction SilentlyContinue)) {
     Write-Host "Creating Temp Directory"
-        Try {
-            $OutNull = New-Item $TempCerts -ItemType Directory
-        } Catch {
-            Write-Host "Failed to make directory!"
-            Write-Host "Error: $_"
-            Exit
-        }
-    } Else {
-        Write-Host "$($TempCerts) exists."
-    }
-    # Get certs from CertHub
     Try {
-        Invoke-WebRequest -Uri "https://$Server/$CertificateAPIURL" -Method GET -Headers @{"apiKey" = "$CertificateAPIKey"} -OutFile "$TempCerts\certchain.crt"
-    } Catch {
-        Write-Host "ERROR: FAILED TO GET CERTIFICATE: $($_)"
+        $null = New-Item $TempCerts -ItemType Directory
     }
+    Catch {
+        Write-Host "Failed to make directory!"
+        Write-Host "Error: $_"
+        Exit 1
+    }
+}
+Else {
+    Write-Host "$($TempCerts) exists."
+}
+
+# Download current LeGo certificate
+Try {
+    Invoke-WebRequest -Uri "https://$Server/$CertificateAPIURL" -Method GET -Headers @{"apiKey" = "$CertificateAPIKey" } -OutFile "$TempCerts\certchain.crt"
+}
+Catch {
+    Write-Host "ERROR: FAILED TO GET CERTIFICATE: $($_)"
+    Exit-Failed
+}
+
+# cert object for LeGo cert
+$legoCert = New-Object Security.Cryptography.X509Certificates.X509Certificate2 "$TempCerts\certchain.crt"
+
+# If LeGo cert has longer validity (or doesn't exist on host yet), update
+If ($CurrentCertExpireTime -lt $legoCert.NotAfter -Or [string]::IsNullOrWhiteSpace($CurrentCertExpireTime)) {
+    Write-Host "Newer certificate available, updating"
+
+    # Remove old cert
+    $null = (Get-ChildItem -Path "Cert:\LocalMachine\My" | Where-Object { $_.Subject -Like "*$CertSubject" }) | Remove-Item -Force
+
     # Get key from CertHub
     Try {
-        Invoke-WebRequest -Uri "https://$Server/$KeyAPIURL" -Method GET -Headers @{"apiKey" = "$KeyAPIKey"} -OutFile "$TempCerts\key.key"
-    } Catch {
+        Invoke-WebRequest -Uri "https://$Server/$KeyAPIURL" -Method GET -Headers @{"apiKey" = "$KeyAPIKey" } -OutFile "$TempCerts\key.key"
+    }
+    Catch {
         Write-Host "ERROR: FAILED TO GET KEY: $($_)"
+        Exit-Failed
     }
 
     # Convert the certificate and private key into a PKCS12 file
@@ -67,9 +89,12 @@ If ($CurrentCertExpireTime -lt $CurrentTime -Or [string]::IsNullOrWhiteSpace($Cu
     # Import the PKCS12 file into the Local Machine Personal certificate store
     Import-PfxCertificate -FilePath $PKCS12Path -Password $EncryptedPassword -CertStoreLocation "cert:\LocalMachine\My"
     
-    # Remove the temp directory
-    Remove-Item -Recurse -Force $TempCerts
-} Else {
-    Write-Host "Certificate is still valid."
-    Exit
+    # Success
+    Write-Host "Certificate updated."
 }
+Else {
+    Write-Host "Certificate is still most recent."
+}
+
+# Remove temp folder
+Remove-Item -Recurse -Force $TempCerts
